3518e97145bc11db745fb60790e4821a0e7bff5e
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-06-12 13:04:26 -0400

    twitter not working but everything else does

--- liri.js
+++ liri.js
@@ -5,8 +5,11 @@
 //grab from keys.js
 var keys = require("./keys.js")
 var Twitter = require('twitter');
+var request = require('request');
+var spotify = require('spotify');
 
-
+var command = process.argv[2];
+var input = process.argv[3];
 
 
 
@@ -17,43 +20,8 @@ var client = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
+// console.log(client)
 
-
-//============================================
-//=============== spotify ====================
-//============================================
-// var spotify = require('spotify');
-//
-// spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-//     if ( err ) {
-//         console.log('Error occurred: ' + err);
-//         return;
-//     }
-//
-//     // Do something with 'data'
-//     search: function({ type: 'artist OR album OR track', query: 'My search query' }, hollaback)
-//     // get: function(query, hollaback) -- See http://developer.spotify.com/en/metadata-api/overview/
-// });
-
-//============================================
-//=============== Request ====================
-//============================================
-// var request = require('request');
-// request('http://www.google.com', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
-//============================================
-//=============== OMDB ====================
-//============================================
-
-// data request url
-//http://www.omdbapi.com/?apikey=[yourkey]&
-
-//poster data request
-//http://img.omdbapi.com/?apikey=[yourkey]&
 
 
 //============================================
@@ -62,7 +30,6 @@ var client = new Twitter({
 
 //MY TWEET
 
-var command = process.argv[2];
 
 if (command === "my-tweets") {
 
@@ -72,19 +39,57 @@ if (command === "my-tweets") {
       };
 
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
             if (!error) {
-
               console.log(tweets)
+            }
 
-            };//end if
 
       });//end client get
 
 }//end if tweet
 
-//spotify this song
 
-//movie-this
+
+
+
+//============================================
+//=============== spotify ====================
+//============================================
+      //spotify this song
+      if (command === "spotify-this-song") {
+        spotify.search({ type: 'track', query: input }, function(err, data) {
+            if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+
+            // Do something with 'data'
+            console.log(data)
+});
+      }
+
+
+
+
+//============================================
+//=============== OMDB ====================
+//============================================
+    //movie-this
+    if (command === "movie-this") {
+
+          //============================================
+          //=============== Request ====================
+          //============================================
+
+          request('http://www.omdbapi.com/?apikey=40e9cece&t=' + input+ '&r=json', function (error, response, body) {
+                if (!error) {
+                  console.log('body:', response.body);
+                  // console.log('statusCode:', response.statusCode);
+                }
+          });
+
+    }
+
+
 
 //do-what-it-says
