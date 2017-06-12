d4a861ebfaa858921002b858e56ded80db80b257
Adam Batimana <adambatimana@Adams-iMac.fios-router.home>
2017-06-12 14:57:27 -0400

    working twitter

--- keys.js
+++ keys.js
@@ -4,8 +4,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '9FXNK9Fi6jtVr2USWtM9IA9Ew',
-  consumer_secret: 'lZVgr1GInv656dLyItLq8LETrxC1tEgE4wTj2cMN6wTyil2lpR',
-  access_token_key: '344162315-AmIGDchgYo1NBsCUNcwZaMDbdGvfJ58azNLXLpde',
-  access_token_secret: 'jQwI5xqTuyJyRSJPCWZ9BTrgPuRdNFfUgd9ZicYVpE2d',
+  consumer_key: 'zMlKZFBdvGDYcq7okKsorxsoy',
+  consumer_secret: 'CZaIxeA0BMCL8U6BzjmSnMcuG7K8A72NdJpO9Mclj0BcumugD4',
+  access_token_key: '844224106446950401-WmaC6CGOCvuJzlGfJiL0RuhyFnMmuYZ',
+  access_token_secret: 'uKvanhcrOKwIJ0z4gkBFRGDvEytWpObZpJlJLNqX2Antt',
 }
--- liri.js
+++ liri.js
@@ -1,28 +1,44 @@
+
+
+//grab from keys.js
+let keys = require("./keys.js")
+let Twitter = require('twitter');
+let request = require('request');
+let spotify = require('spotify');
+const fileName = 'log.txt'
+const fs = require('fs')
+let command = process.argv[2];
+let input = process.argv[3];
+let contents = 'Log all the information here! ';
+
+//============================================
+//=============== read and write =============
+//============================================
+fs.writeFile(fileName, contents, function(err){
+  if (err) {
+    console.log(err)
+  }
+})
+
+fs.readFile(fileName, 'utf8', function(err,data){
+  if (err) {
+    console.log(err)
+  }
+
+  const logInfo = data.split(',')
+})
+
 //============================================
 //=============== twitter ====================
 //============================================
-
-//grab from keys.js
-var keys = require("./keys.js")
-var Twitter = require('twitter');
-var request = require('request');
-var spotify = require('spotify');
-
-var command = process.argv[2];
-var input = process.argv[3];
-
-
-
-var client = new Twitter({
-  consumer_key: keys.twitterKeys.consumer_key,
-  consumer_secret: keys.twitterKeys.consumer_secret,
-  access_token_key: keys.twitterKeys.access_token_key,
-  access_token_secret: keys.twitterKeys.access_token_secret
-});
+    let client = new Twitter({
+      consumer_key: keys.twitterKeys.consumer_key,
+      consumer_secret: keys.twitterKeys.consumer_secret,
+      access_token_key: keys.twitterKeys.access_token_key,
+      access_token_secret: keys.twitterKeys.access_token_secret
+    });
 
 // console.log(client)
-
-
 
 //============================================
 //=============== liri Bot ===================
@@ -34,14 +50,19 @@ var client = new Twitter({
 if (command === "my-tweets") {
 
       var params = {
-        screen_name: 'allaboutmada',
+        screen_name: 'adambatimana',
         count:20
       };
 
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
-            if (!error) {
-              console.log(tweets)
-            }
+        if (!error) {
+      for (i=0; i<tweets.length; i++) {
+          var returnedData = ('Number: ' + (i+1) + '\n' + tweets[i].created_at + '\n'+ '@' +tweets[i].user.name+ ': ' + tweets[i].text + '\n');
+          console.log(returnedData);
+          console.log("-------------------------");
+
+      }
+  };
 
 
       });//end client get
@@ -56,7 +77,7 @@ if (command === "my-tweets") {
 //=============== spotify ====================
 //============================================
       //spotify this song
-      if (command === "spotify-this-song") {
+  else if (command === "spotify-this-song") {
         spotify.search({ type: 'track', query: input }, function(err, data) {
             if ( err ) {
                 console.log('Error occurred: ' + err);
@@ -68,14 +89,11 @@ if (command === "my-tweets") {
 });
       }
 
-
-
-
-//============================================
-//=============== OMDB ====================
-//============================================
-    //movie-this
-    if (command === "movie-this") {
+      //============================================
+      //=============== OMDB ====================
+      //============================================
+          //movie-this
+      else if (command === "movie-this") {
 
           //============================================
           //=============== Request ====================
@@ -84,12 +102,48 @@ if (command === "my-tweets") {
           request('http://www.omdbapi.com/?apikey=40e9cece&t=' + input+ '&r=json', function (error, response, body) {
                 if (!error) {
                   console.log('body:', response.body);
-                  // console.log('statusCode:', response.statusCode);
+                  console.log('statusCode:', response.statusCode);
                 }
+
+
+                fs.appendFile(fileName, ", " + body, function(err){
+                  if (err) {
+                    console.log(err)
+                  }
+                })
           });
 
-    }
+    }//do-what-it-says
+      else if (command === "do-what-it-says") {
+        fs.readFile('random.txt', 'utf8', function(err, data){
+          if(err){
+            console.log(err)
+          }
+          fs.appendFile(fileName, ", " + data, function(err){
+            if (err) {
+              console.log(err)
+            }
+          })
+          console.log(data)
+
+        })
 
 
+  }
 
-//do-what-it-says
+  else if (command === "movie-this" && !input){
+    request('http://www.omdbapi.com/?apikey=40e9cece&t=mr+nobody&r=json', function (error, response, body) {
+          if (!error) {
+            console.log('body:', response.body);
+            console.log('statusCode:', response.statusCode);
+          }
+
+          console.log("if you have not watched MR. Nobody, then you should")
+
+          fs.appendFile(fileName, ", " + body, function(err){
+            if (err) {
+              console.log(err)
+            }
+          })
+    });
+  }
